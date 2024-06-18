e80dd4f09a820d70e1cefe1f44c94aaec46309f2
Caio <bukvar2009@gmail.com>
2024-06-18 08:30:05 -0300

    feat: adding banners for mobile and desktop version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import BannerSmall from "@/assets/images/banner-small.png";
+import BannerLarge from "@/assets/images/banner-large.png";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <Header />
-      <div>
-        <p>batata</p>
+      <div className="relative w-full h-64">
+        <picture>
+          <source srcSet={BannerLarge.src} media="(min-width: 1024px)" />
+          <img
+            src={BannerSmall.src}
+            alt="Banner"
+            className="w-full h-[150px]"
+          />
+        </picture>
       </div>
     </main>
   );
