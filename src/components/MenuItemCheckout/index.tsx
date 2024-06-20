001874e7f3324705fbaf0ddeb02ad6afb14ff278
Caio <bukvar2009@gmail.com>
2024-06-20 13:07:16 -0300

    feat: creation of layout for checkout modifiers - no integration yet

--- src/components/MenuItemCheckout/index.tsx
+++ src/components/MenuItemCheckout/index.tsx
@@ -4,6 +4,9 @@ import * as Dialog from "@radix-ui/react-dialog";
 import Image from "next/image";
 import { useSelector } from "react-redux";
 import CloseButton from "../CloseButton";
+import MinusBtn from "@/assets/images/minusButton.svg";
+import PlusBtn from "@/assets/images/plusButton.svg";
+import { useState } from "react";
 
 const MenuItemCheckout: React.FC<MenuItemCheckoutProps> = ({
   venue,
@@ -12,8 +15,27 @@ const MenuItemCheckout: React.FC<MenuItemCheckoutProps> = ({
   const isCheckoutOpen = useSelector(
     (state: RootState) => state.isMenuCheckoutOpen
   );
+  const [desiredAmount, setDesiredAmount] = useState(0);
+  const [selectedOptions, setSelectedOptions] = useState<{
+    [key: number]: number;
+  }>({});
 
-  console.log("selectedItem", selectedItem);
+  const handleAmount = (type: string) => {
+    if (type === "minus") {
+      setDesiredAmount(desiredAmount <= 0 ? 0 : desiredAmount - 1);
+    }
+    if (type === "plus") {
+      setDesiredAmount(desiredAmount + 1);
+    }
+  };
+
+  const handleRadioChange = (modifierId: number, itemId: number) => {
+    setSelectedOptions((prevState) => ({
+      ...prevState,
+      [modifierId]: itemId,
+    }));
+  };
+  console.log(selectedItem.modifiers?.map((modifier) => console.log(modifier)));
 
   return (
     <Dialog.Root open={isCheckoutOpen.isMenuCheckoutOpen}>
@@ -48,28 +70,74 @@ const MenuItemCheckout: React.FC<MenuItemCheckoutProps> = ({
               )}
             </div>
 
-            <div className="flex flex-col bg-[#F8F9FA] p-4">
-              <p className="text-[#464646] text-[16px] font-[700]">
-                Choose your size
-              </p>
-              <p className="text-[#464646] text-[16px] font-[400]">
-                Select 1 option
-              </p>
+            {selectedItem.modifiers && (
+              <div className="flex flex-col bg-[#F8F9FA]">
+                {selectedItem.modifiers.map((modifier) => (
+                  <>
+                    <div key={modifier.id} className="p-4">
+                      <p className="text-[#464646] text-[16px] font-[700]">
+                        {modifier.name}
+                      </p>
+                      <p className="text-[#464646] text-[16px] font-[400]">
+                        Select 1 option
+                      </p>
+                    </div>
 
-              {/* {selectedItem.modifiers?.map((modifier) => {
-                <p>aa</p>;
-              })} */}
-            </div>
+                    <div className="bg-white px-4">
+                      {modifier.items.map((item) => (
+                        <label
+                          key={item.id}
+                          onClick={() =>
+                            handleRadioChange(modifier.id, item.id)
+                          }
+                          className="flex w-full justify-between items-center cursor-pointer"
+                        >
+                          <div className="flex flex-col items-start justify-center h-[72px] gap-1">
+                            <p className="text-[#121212] font-[500]">
+                              {item.name}
+                            </p>
+                            <p className="text-[#464646]">
+                              R${item.price.toFixed(2)}
+                            </p>
+                          </div>
+                          <input
+                            type="radio"
+                            name={`modifier-${modifier.id}`}
+                            value={item.id}
+                            checked={selectedOptions[modifier.id] === item.id}
+                            className="round-checkbox"
+                            readOnly
+                          />
+                        </label>
+                      ))}
+                    </div>
+                  </>
+                ))}
+              </div>
+            )}
 
-            <div className="flex w-full flex-col items-center p-4">
-              <div className="flex gap-4">
-                <button>-</button>
-                <p>qte</p>
-                <button>+</button>
+            <div className="flex w-full flex-col items-center p-4 gap-[10px]">
+              <div className="flex gap-4 ">
+                <button
+                  onClick={() => handleAmount("minus")}
+                  className="flex items-center justify-center size-8 rounded-full bg-[#DADADA]"
+                >
+                  <Image src={MinusBtn} alt="minus" />
+                </button>
+                <p className="font-bold self-center text-[24px] h-[32px]">
+                  {desiredAmount}
+                </p>
+                <button
+                  onClick={() => handleAmount("plus")}
+                  className="flex items-center justify-center size-8 rounded-full bg-[#4F372F]"
+                >
+                  <Image src={PlusBtn} alt="plus" />
+                </button>
               </div>
 
               <button
-                className="py-1 px-6 rounded-[40px text-[18px] text-white rounded-[40px] w-full"
+                className="py-1 px-6 rounded-[40px text-[18px]
+                text-white rounded-[40px] w-full font-[400]"
                 style={{ backgroundColor: venue?.webSettings.primaryColour }}
               >
                 Add to order • R${selectedItem.price.toFixed(2)}
